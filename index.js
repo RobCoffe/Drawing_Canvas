234409f0aa29f081220ad06ff4f33ab846ae5e9f
Robson Junior <111658874+RobCoffe@users.noreply.github.com>
2023-01-06 21:56:14 -0300

    Criação do JS, para captação do movimento do mouse,(Creation of JavaScript to capture mouse movement)

---
+++ index.js
@@ -0,0 +1,14 @@
+const canvas = document.getElementById('drawing-board');
+const toolbar = document.getElementById('ferramentas');
+const ctx = canvas.getContext('2d');
+
+const canvasOffsetX = canvas.offsetLeft;
+const canvasOffsetY = canvas.offsetTop;
+
+canvas.width = window.innerWidth - canvasOffsetX;
+canvas.height = window.innerHeight - canvasOffsetY;
+
+let isPainting = false;
+let lineWidth = 5;
+let startX;
+let startY;
