1ebdcaedb702dd96dd843bd2a2f5145adce526a4
Robson Junior <111658874+RobCoffe@users.noreply.github.com>
2023-01-07 09:56:03 -0300

    Adicione event.listener para capturar as ações. (Add event.listener for catch the actions.)

--- index.js
+++ index.js
@@ -12,3 +12,25 @@ let isPainting = false;
 let lineWidth = 5;
 let startX;
 let startY;
+
+ferramentas.addEventListener('click', e => {
+    if (e.target.id === 'limpar') {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+});
+
+ferramentas.addEventListener('change', e => {
+    if(e.target.id === 'paleta') {
+        ctx.strokeStyle = e.target.value;
+    }
+
+    if(e.target.id === 'lineWidth') {
+        lineWidth = e.target.value;
+    }
+    
+});
+
+const draw = (e) => {
+    if(!isPainting) {
+        return;
+    }
